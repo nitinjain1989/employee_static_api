9cde1d320e8e6d4c710acedace377e84c73caeae
Nitin Jain <nitin.jain1989@outlook.com>
2026-02-10 15:47:03 +0530

    update html

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:1.25-alpine AS builder
 WORKDIR /app
-COPY go.mod ./
+COPY go.mod go.sum ./
 RUN go mod download
 COPY . .
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o app
@@ -8,6 +8,7 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o app
 FROM gcr.io/distroless/base-debian12
 WORKDIR /app
 COPY --from=builder /app/app .
+COPY --from=builder /app/public ./public  
 EXPOSE 8080
 CMD ["/app/app"]
 
--- main.go
+++ main.go
@@ -4,19 +4,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"static-api/handlers"
 )
 
 func main() {
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	// API routes
 	http.HandleFunc("/employees", handlers.GetEmployees)
 	http.HandleFunc("/employees/create", handlers.CreateEmployee)
-	log.Println("Server started on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// Absolute path fix (IMPORTANT)
+	publicPath, err := filepath.Abs("public")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Serving static files from:", publicPath)
+
+	http.Handle("/", http.FileServer(http.Dir(publicPath)))
+
+	log.Println("Server started on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
