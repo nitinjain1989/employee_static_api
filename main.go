1962e809ca2b6c94d993709d557bfc84c1088ccc
Nitin Jain <nitin.jain1989@outlook.com>
2026-02-10 11:36:47 +0530

    add port code

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"static-api/handlers"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	http.HandleFunc("/employees", handlers.GetEmployees)
 	log.Println("Server started on http://localhost:8080")
