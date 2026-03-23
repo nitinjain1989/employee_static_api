a162dc5f105b63ae6b23459433ae262c5c102f2f
Nitin Jain <nitin.jain1989@outlook.com>
2026-03-23 15:05:03 +0530

    add production swagger

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"static-api/routes"
 
 	"github.com/joho/godotenv"
+
+	docs "static-api/docs"
 )
 
 func init() {
@@ -26,13 +28,21 @@ func main() {
 		port = "8080"
 	}
 
+	docs.SwaggerInfo.Title = "Employee API"
+	docs.SwaggerInfo.BasePath = "/api"
+	docs.SwaggerInfo.Description = "Employee Management Service"
+	docs.SwaggerInfo.Version = "1.0"
+	env := os.Getenv("ENV")
+	if env == "production" {
+		docs.SwaggerInfo.Host = "https://employee-static-api.onrender.com"
+		docs.SwaggerInfo.Schemes = []string{"https"}
+	} else {
+		docs.SwaggerInfo.Host = "localhost:8080"
+		docs.SwaggerInfo.Schemes = []string{"http"}
+	}
+
 	// API routes
 	router := routes.RegisterRoutes()
-
-	/*	// ✅ Serve public folder
-		fs := http.FileServer(http.Dir("./public"))
-		http.Handle("/", fs)
-		http.Handle("/api/", router)*/
 
 	log.Println("Server running on :" + port)
 	log.Fatal(router.Run(":" + port))
